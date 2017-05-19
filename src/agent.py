599bccf2529cad46591faecb3cd74bba7334e373
akashin <kashin.andrej@gmail.com>
2017-05-19 20:54:15 +0300

    Start implementing A3C

--- src/agent.py
+++ src/agent.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 
 from .base import BaseModel
 from .history import History
-from .ops import linear, conv2d
+from .ops import linear, conv2d, batch_sample
 from .utils import get_time
 
 from tensorflow.contrib.framework.python.ops import variables
@@ -26,7 +26,8 @@ class Agent(BaseModel):
 
     self.step_op = tf.contrib.framework.get_or_create_global_step()
     self.step_inc_op = self.step_op.assign_add(1, use_locking=True)
-    self.build_dqn()
+    # self.build_dqn()
+    self.build_a3c()
 
     self.saver = tf.train.Saver(list(self.w.values()) + [self.step_op], max_to_keep=30)
 
@@ -199,6 +200,110 @@ class Agent(BaseModel):
     self.batch_reward = []
     self.batch_action = []
     self.batch_terminal = []
+
+  def build_a3c(self):
+    self.w = {}
+    self.t_w = {}
+
+    initializer = tf.truncated_normal_initializer(0, 0.02)
+    activation_fn = tf.nn.relu
+    DQN_type = 'nature'
+    data_format = self.cnn_format
+
+    if data_format == 'NHWC':
+      self.s_t = tf.placeholder('float32',
+          [None, self.screen_width, self.screen_height, self.history_length], name='s_t')
+    elif data_format == 'NCHW':
+      self.s_t = tf.placeholder('float32',
+          [None, self.history_length, self.screen_width, self.screen_height], name='s_t')
+
+    if data_format == 'NCHW':
+      device = '/gpu:0'
+    elif data_format == 'NHWC':
+      device = '/cpu:0'
+    else:
+      raise ValueError('Unknown data_format: %s' % data_format)
+
+    if DQN_type.lower() == 'nature':
+      with tf.variable_scope('Nature_DQN'), tf.device(device):
+        self.w = {}
+
+        self.l0 = tf.div(self.s_t, 255.)
+        self.l1, self.w['l1_w'], self.w['l1_b'] = conv2d(self.l0,
+            32, [8, 8], [4, 4], initializer, activation_fn, data_format, name='l1_conv')
+        self.l2, self.w['l2_w'], self.w['l2_b'] = conv2d(self.l1,
+            64, [4, 4], [2, 2], initializer, activation_fn, data_format, name='l2_conv')
+        self.l3, self.w['l3_w'], self.w['l3_b'] = conv2d(self.l2,
+            64, [3, 3], [1, 1], initializer, activation_fn, data_format, name='l3_conv')
+        self.l4, self.w['l4_w'], self.w['l4_b'] = \
+            linear(self.l3, 512, activation_fn=activation_fn, name='l4_linear')
+    elif DQN_type.lower() == 'nips':
+      with tf.variable_scope('Nips_DQN'), tf.device(device):
+        self.w = {}
+
+        self.l0 = tf.div(self.s_t, 255.)
+        self.l1, self.w['l1_w'], self.w['l1_b'] = conv2d(self.l0,
+            16, [8, 8], [4, 4], initializer, activation_fn, data_format, name='l1_conv')
+        self.l2, self.w['l2_w'], self.w['l2_b'] = conv2d(self.l1,
+            32, [4, 4], [2, 2], initializer, activation_fn, data_format, name='l2_conv')
+        self.l4, self.w['l4_w'], self.w['l4_b'] = \
+            linear(self.l2, 256, activation_fn=activation_fn, name='l4_linear')
+    else:
+      raise ValueError('Wrong DQN type: %s' % DQN_type)
+
+    def reshape_w(w):
+      shape = w.get_shape().as_list()
+      return tf.transpose(tf.reshape(w, shape[:2] + [1, -1]), [3, 0, 1, 2])
+
+    # Policy head.
+    with tf.variable_scope('policy'):
+      # 512 -> action_size
+      self.policy_logits, self.w['p_w'], self.w['p_b'] = linear(self.l4, action_size, name='linear')
+
+      with tf.variable_scope('policy'):
+        self.policy = tf.nn.softmax(self.policy_logits, name='pi')
+      with tf.variable_scope('log_policy'):
+        self.log_policy = tf.log(self.policy)
+      with tf.variable_scope('policy_entropy'):
+        self.policy_entropy = -tf.reduce_sum(self.policy * self.log_policy, 1)
+
+      with tf.variable_scope('pred_action'):
+        self.sampled_action = batch_sample(self.policy)
+        sampled_action_one_hot = tf.one_hot(self.sampled_action, action_size, 1., 0.)
+      with tf.variable_scope('log_policy_of_action'):
+        self.log_policy_of_sampled_action = tf.reduce_sum(self.log_policy * sampled_action_one_hot, 1)
+
+    # Value head.
+    with tf.variable_scope('value'):
+      # 512 -> 1
+      self.value, self.w['q_w'], self.w['q_b'] = linear(self.l4, 1, name='linear')
+
+    with tf.variable_scope('optim'):
+      self.R = tf.placeholder('float32', [None], name='target_reward')
+      self.true_log_policy = tf.placeholder('float32', [None], name='true_action')
+
+      # TODO: equation on paper and codes of other implementations are different
+      with tf.variable_scope('policy_loss'):
+        self.policy_loss = -(self.true_log_policy \
+            * (self.R - self.value)) - beta * self.policy_entropy
+
+      with tf.variable_scope('value_loss'):
+        self.value_loss = tf.pow(self.R - self.value, 2) / 2
+
+      with tf.variable_scope('total_loss'):
+        self.total_loss = self.policy_loss + self.value_loss
+
+    # if global_network != None:
+    if True:
+      with tf.variable_scope('copy_from_target'):
+        copy_ops = []
+
+        for name in self.w.keys():
+          copy_op = self.w[name].assign(global_network.w[name])
+          copy_ops.append(copy_op)
+
+        self.global_copy_op = tf.group(*copy_ops, name='global_copy_op')
+
 
   def build_dqn(self):
     self.w = {}
